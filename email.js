957c8c01e5cc9911a354beecca12114a15758657
ChisaeHwang <cwh73090@naver.com>
2023-08-13 03:08:52 +0900

    event crash delete

--- email.js
+++ email.js
@@ -3,29 +3,28 @@ import emailjs from "emailjs-com";
 // emailjs 초기화
 emailjs.init("chisae");
 
-const sendEmail = () => {
-  let templateParams = {
-    email: document.getElementById("emailInput").value,
-  };
+document.getElementById("submitButton").addEventListener("click", function (e) {
+  e.preventDefault(); // form 전송을 막기 위해
 
-  console.log(templateParams);
+  var emailValue = document.getElementById("emailInput").value;
 
-  emailjs.send("chisae_gmail", "template_6h2hhzo", templateParams).then(
-    function (response) {
-      console.log("Success!", response.status, response.text);
-      alert("이메일이 성공적으로 전송되었습니다!");
-    },
-    function (error) {
-      console.log("Failed...", error);
-      alert("이메일 전송 실패. 다시 시도해 주세요.");
-    }
-  );
-};
+  if (emailValue) {
+    let templateParams = {
+      email: emailValue,
+    };
 
-// 버튼 클릭 이벤트 추가
-document
-  .querySelector("button[type='submit']")
-  .addEventListener("click", function (e) {
-    e.preventDefault(); // form 전송을 막기 위해
-    sendEmail();
-  });
+    emailjs.send("chisae_gmail", "template_6h2hhzo", templateParams).then(
+      function (response) {
+        console.log("Success!", response.status, response.text);
+        alert("이메일이 성공적으로 전송되었습니다!");
+        document.getElementById("emailInput").value = ""; // 이메일 입력값 지우기
+      },
+      function (error) {
+        console.log("Failed...", error);
+        alert("이메일 전송 실패. 다시 시도해 주세요.");
+      }
+    );
+  } else {
+    alert("이메일을 입력하세요.");
+  }
+});
